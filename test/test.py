6f270aef36befffdcfcd57a26ad00034c7f38b90
KroegerP <peter14mail@gmail.com>
2023-02-05 17:45:51 -0500

    feat(tkinter): Added test for image add

--- test/test.py
+++ test/test.py
@@ -2,4 +2,11 @@ import tkinter
 root = tkinter.Tk()
 root.title("Fenster 1")
 root.geometry("100x100")
+bg = tkinter.PhotoImage(file="assets/view.png")
+evaFace = tkinter.PhotoImage(file="evaFace4Home.png")
+
+my_canvas = tkinter.Canvas(root, width=1280, height=800)
+my_canvas.pack(fill="both", expand=True)
+
+my_canvas.create_image(0, 0, image=bg, anchor="nw")
 root.mainloop()
