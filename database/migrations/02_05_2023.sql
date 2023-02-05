66eeab62aaee0bc4db4be5c8bfb585ab90b96dde
KroegerP <peter14mail@gmail.com>
2023-02-05 16:29:08 -0500

    feat(db): Deleted some extra records

---
+++ database/migrations/02_05_2023.sql
@@ -0,0 +1,15 @@
+-- Auto-generated SQL script #202302051615
+DELETE FROM public.medicine1
+	WHERE id=74;
+DELETE FROM public.medicine1
+	WHERE id=75;
+DELETE FROM public.medicine1
+	WHERE id=76;
+DELETE FROM public.medicine1
+	WHERE id=77;
+DELETE FROM public.medicine1
+	WHERE id=78;
+DELETE FROM public.medicine1
+	WHERE id=79;
+DELETE FROM public.medicine1
+	WHERE id=7;
