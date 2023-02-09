92da7571f7c4c791df4b38673fc3efdfd4a43156
Peter Kroeger <peter14mailgmail.com>
2023-02-09 16:18:19 -0500

    maint(tkinter): Clean up comments

--- test/test.py
+++ test/test.py
@@ -4,15 +4,7 @@ import os, sys
 
 path = os.path.dirname(__file__)
 
-# new_path = os.path.join(os.path.abspath(os.getcwd()), 'test/')
-
 print(os.path.abspath(os.getcwd()))
-
-# os.path.join(os.path.dirname(__file__),'~/projects/EVAProject/EXPOFILES/')
-# sys.path.append(new_path)
-# print(os.path.join(os.path.dirname(__file__),'/assets'))
-# print(os.path.dirname(__file__))
-# print(os.path.abspath(os.getcwd()))
 
 pprint(sys.path)
 
@@ -23,10 +15,7 @@ root.title("Fenster 1")
 root.geometry("500x500")
 bg = tkinter.PhotoImage(name="View",file=f'{file_name}/view.png')
 bg = tkinter.PhotoImage(name="View2",file='test/assets/view.png')
-# bg = tkinter.PhotoImage(name="View3",file='assets/view.png')
 evaFace = tkinter.PhotoImage(file="test/assets/evaFace4Home.png")
-# bg = tkinter.PhotoImage(file="~/projects/EVAProject/EXPOFILES/assets/view.png")
-# evaFace = tkinter.PhotoImage(file="~/projects/EVAProject/EXPOFILES/assets/evaFace4Home.png")
 
 my_canvas = tkinter.Canvas(root, width=1280, height=800)
 my_canvas.pack(fill="both", expand=True)
