23e040a3031c16e71a44c131131f9cc1aa902685
Peter Kroeger <peter14mailgmail.com>
2023-02-10 16:48:42 -0500

    feat(db): Add seed data, split up transactions

--- database/migrations/02_10_2023.sql
+++ database/migrations/02_10_2023.sql
@@ -1,3 +1,5 @@
+BEGIN TRAN T1;
+
 CREATE TABLE public.medications (
 	id int NOT NULL,
 	medname varchar NOT NULL DEFAULT Unknown,
@@ -18,4 +20,38 @@ CREATE TABLE public.confirmations (
 	CONSTRAINT confirmations_pk PRIMARY KEY (id)
 );
 
+ALTER TABLE public.confirmations ADD created_at date NULL DEFAULT NOW();
+
+COMMIT TRAN T1;
+
+BEGIN TRAN T2;
+
 ALTER TABLE public.confirmations ADD CONSTRAINT confirmations_fk FOREIGN KEY (medicationid,medname) REFERENCES public.medications(id,medname);
+
+-- Auto-generated SQL script #202302101613
+INSERT INTO public.medications (id,medname,refillsleft,timesperday,timesperweek,folderpath)
+	VALUES (1,'med',2,2,7,'meds/med/1/');
+
+COMMIT TRAN T2;
+
+BEGIN TRAN T3;
+
+INSERT INTO public.confirmations (id,medname,taken,medicationid,created_at) VALUES
+	 (2,'med',true,1,'2023-02-10'),
+	 (3,'med',false,1,'2023-02-10'),
+	 (4,'med',false,1,'2023-02-10'),
+	 (5,'med',true,1,'2023-02-10'),
+	 (1,'med',false,1,'2023-02-10'),
+	 (6,'med',true,1,'2023-02-10'),
+	 (7,'med',true,1,'2023-02-02'),
+	 (8,'med',false,1,'2023-02-02'),
+	 (9,'med',true,1,'2023-02-02'),
+	 (10,'med',false,1,'2023-02-02');
+
+INSERT INTO public.confirmations (id,medname,taken,medicationid,created_at) VALUES
+	 (11,'med',false,1,'2023-02-02'),
+	 (12,'med',true,1,'2023-02-02'),
+	 (13,'med',true,1,'2023-02-02'),
+	 (14,'med',true,1,'2023-02-02');
+
+COMMIT TRAN T3;
