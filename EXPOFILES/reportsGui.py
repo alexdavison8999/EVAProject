6a9f7816a2a8212e0b0ac4c2e0361b1bc1030390
Peter Kroeger <peter14mailgmail.com>
2023-02-10 15:20:16 -0500

    fix(report): Trying to get images working

--- EXPOFILES/reportsGui.py
+++ EXPOFILES/reportsGui.py
@@ -9,11 +9,18 @@ from constants.window import *
 # Ideas from this could be useful later: https://stackoverflow.com/questions/20399243/display-message-when-hovering-over-something-with-mouse-cursor-in-python
 # https://www.tutorialspoint.com/how-to-clear-tkinter-canvas#:~:text=While%20creating%20a%20canvas%20in,present%20in%20a%20tkinter%20frame.
 
+# Issues related to old tkinter bug that functions will not load image paths due to garbage collection
+# https://stackoverflow.com/questions/16424091/why-does-tkinter-image-not-show-up-if-created-in-a-function
+
+
 confir = 4
 
 def report2command():
 	# loadingReportGui2()
 	pass
+
+def clickFunc():
+	print("Clicked!")
 
 # hides the home GUI and updates the canvas with the reports GUI
 def reportGui(GuiClass, classCanvas: tk.Canvas) -> None:
@@ -36,18 +43,18 @@ def reportGui(GuiClass, classCanvas: tk.Canvas) -> None:
 		reports_label = tk.Label(classCanvas, text='Reports', bg='#F0F8FF', font=('arial', 40, 'normal'))
 
 		# Creating a photoimage object to use image
-		imPath = "EXPOFILES/assets/report1.png"
-		photo = tk.PhotoImage(file=imPath)
+		report_image = tk.PhotoImage(file="EXPOFILES/assets/report1.png")
 
 		cog_report_btn = UI.NewHomeBtn(master=classCanvas, text='Cognitive Report', command=report2command)
 		go_back_btn = UI.NewHomeBtn(master=classCanvas, text='Go Back', command=cleanupReports)
-		click_me_btn = tk.Button(classCanvas, text='Click Me!', image=photo)
+		# click_me_btn = tk.Button(classCanvas, image=report_image, command=clickFunc)
 
 		GuiClass.canvasIds["Report"] = []
-		GuiClass.canvasIds["Report"].append(GuiClass.canvas.create_window(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 20, window=reports_label, anchor=tk.N))
-		GuiClass.canvasIds["Report"].append(GuiClass.canvas.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, window=cog_report_btn, anchor=tk.SE))
-		GuiClass.canvasIds["Report"].append(GuiClass.canvas.create_window(WINDOW_PADDING, WINDOW_HEIGHT_PADDING, window=go_back_btn, anchor=tk.SW))
-		# GuiClass.canvasIds["Report"].append(GuiClass.canvas.create_window(WINDOW_WIDTH / 4, 700, window=click_me_btn, anchor=tk.S))
+		GuiClass.canvasIds["Report"].append(classCanvas.create_window(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 20, window=reports_label, anchor=tk.N))
+		GuiClass.canvasIds["Report"].append(classCanvas.create_window(WINDOW_WIDTH_PADDING, WINDOW_HEIGHT_PADDING, window=cog_report_btn, anchor=tk.SE))
+		GuiClass.canvasIds["Report"].append(classCanvas.create_window(WINDOW_PADDING, WINDOW_HEIGHT_PADDING, window=go_back_btn, anchor=tk.SW))
+		# GuiClass.canvasIds["Report"].append(classCanvas.create_window(WINDOW_PADDING / 2, WINDOW_HEIGHT_PADDING / 2, window=click_me_btn))
+		GuiClass.canvasIds["Report"].append(classCanvas.create_image(0, 0 / 2, image=report_image, anchor="nw"))
 
 	else:
 		print("Error creating Report screen")
