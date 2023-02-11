43dc1be02514b174c460e6ed611c40d6f4850f88
Peter Kroeger <peter14mailgmail.com>
2023-02-11 11:09:55 -0500

    feat(env): Added hinting to resetWindow

--- EXPOFILES/utils/itemHelpers.py
+++ EXPOFILES/utils/itemHelpers.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import TypedDict
 
 # from utils.appLocation import AppLocation
 
@@ -6,7 +7,16 @@ APP_LOCATIONS = ['Home', 'Drug Info', 'Report', 'Confirm']
 
 # Function to delete all the UI from the canvas given a specific location
 # Returns False if the given app location is not in the list above
-def resetWindow(canvas: tk.Canvas, canvasIds: list[int],appLocation: str) -> bool:
+def resetWindow(canvas: tk.Canvas, canvasIds: TypedDict,appLocation: str) -> bool:
+    """
+    Method to cleanup objects on the current canvas
+    Inputs:
+        canvas:         Tkinter canvas object
+        canvasIds:      Id's object containing the ID's on the canvas
+        appLocation:    The location in which the app is LEAVING, meaning that the
+                        method will cleanup objects that have been added to the
+                        `appLocation` key of the canvasIds dictionary
+    """
     if appLocation not in APP_LOCATIONS:
         print(f"ERROR: App location {appLocation} not present in {APP_LOCATIONS}!")
         return False
