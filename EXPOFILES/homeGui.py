9d24c7380a0f79ae3ca59d13fffc18804fc9deaf
Peter Kroeger <peter14mailgmail.com>
2023-02-13 19:59:59 -0500

    feat(home): Remov background, add new icons

--- EXPOFILES/homeGui.py
+++ EXPOFILES/homeGui.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from datetime import datetime
+import functools
 # PACKAGES
 import tkinter as tk
 from time import strftime
@@ -29,29 +31,33 @@ def homeGui(UIController: UIController):
     global background
     global eva_face
     # Initializing assets
-    background = tk.PhotoImage(file='EXPOFILES/assets/view.png')
-    eva_face = tk.PhotoImage(file="EXPOFILES/assets/evaFace4Home.png")
+    # background = tk.PhotoImage(file='EXPOFILES/assets/view.png')
+    eva_face = tk.PhotoImage(file="EXPOFILES/assets/evaFaceRedLarge.png")
 
     # TODO: Maybe just rewrite this string depending on where we are rather than making new text to manage?
     eva_text = "Hi I'm Eva.\nHow can I help you?"
 
+    date = datetime.now().strftime("%H %M")
+    date = date.split(" ")
+    print(date[0], date[1])
+
     # Program Navigation Buttons
     scan_btn = UI.NewHomeBtn(master=UIController.canvas, text='Scan Bottle', command=UIController.scanSelect)
     drug_info_btn = UI.NewHomeBtn(master=UIController.canvas, text='Drug Info', command=UIController.goToDrugInfo)
-    confirm_btn = UI.NewHomeBtn(master=UIController.canvas, text='Daily Confirmation', command=UIController.goToConfirm)
+    confirm_btn = UI.NewHomeBtn(master=UIController.canvas, text='Daily Confirmation', command=functools.partial(UIController.goToConfirm, hour=date[0], minute=date[1]))
     report_btn = UI.NewHomeBtn(master=UIController.canvas, text='Reports', command=UIController.goToReport)
     exit_btn = UI.NewHomeBtn(master=UIController.canvas, text='Exit', command=UIController.closeEVA)
 
     # Adding assets to the canvas and the canvasIds list
     # These can be used to control the visibility of items
-    UIController.canvasIds["Home"].append(UIController.canvas.create_image(
-        0, 0, image=background, anchor="nw"
-    ))
+    # UIController.canvasIds["Home"].append(UIController.canvas.create_image(
+    #     0, 0, image=background, anchor="nw"
+    # ))
     UIController.canvasIds["Home"].append(UIController.canvas.create_image(
         0, 0, image=eva_face, anchor="nw"
         ))
     UIController.canvasIds["Home"].append(UIController.canvas.create_window(
-        70, 530, anchor='nw', window=UIController.clock_text
+        WINDOW_WIDTH_PADDING, WINDOW_HEIGHT_PADDING, anchor='se', window=UIController.clock_text
     ))
     UIController.canvasIds["Home"].append(UIController.canvas.create_text(
         300, 250, text=eva_text, font=("Roboto", 40), fill="black"
@@ -60,5 +66,5 @@ def homeGui(UIController: UIController):
     UIController.canvasIds["Home"].append(UIController.canvas.create_window(WINDOW_WIDTH_PADDING,300,window=drug_info_btn, anchor=tk.E))
     UIController.canvasIds["Home"].append(UIController.canvas.create_window(WINDOW_WIDTH_PADDING,400,window=confirm_btn, anchor=tk.E))
     UIController.canvasIds["Home"].append(UIController.canvas.create_window(WINDOW_WIDTH_PADDING,500,window=report_btn, anchor=tk.E))
-    UIController.canvasIds["Home"].append(UIController.canvas.create_window(0 ,800,window=exit_btn, anchor=tk.SW))
+    UIController.canvasIds["Home"].append(UIController.canvas.create_window(WINDOW_PADDING ,WINDOW_HEIGHT_PADDING,window=exit_btn, anchor=tk.SW))
     
