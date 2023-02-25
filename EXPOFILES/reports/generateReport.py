673408b02afb0713e8f0c5a46458c159881a9491
Peter Kroeger <peter14mailgmail.com>
2023-02-25 14:04:37 -0500

    WIP(report): Image axes formatting

--- EXPOFILES/reports/generateReport.py
+++ EXPOFILES/reports/generateReport.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+import pandas as pd
+import datetime as dt
 
 import plotly.express as px
 
@@ -11,6 +13,11 @@ def generateReport(conn: psycopg2.extensions.connection, medName: str,) -> str:
     file_path = ''
 
     confirm_data = {"date": [], "time": [], "taken": []}
+    hours = []
+
+    [hours.append(f'{num}:00') for num in range(24)]
+
+    hours = pd.to_datetime(hours).time
 
     confirm_tuples = getConfirmationsByMedName(conn, medName)
 
@@ -24,9 +31,23 @@ def generateReport(conn: psycopg2.extensions.connection, medName: str,) -> str:
             created_time = confirm.createdAt.strftime("%H:%M:%S")
             created_time_int = int(confirm.createdAt.strftime("%H%M"))
 
-            confirm_data["date"].append(created_date)
+            print(confirm.createdAt.strftime("%X"))
+
+            confirm_data["date"].append(confirm.createdAt)
             confirm_data["time"].append(created_time_int)
             confirm_data["taken"].append(confirm.taken)
+
+
+        confirm_data["date"] = pd.to_datetime(confirm_data["date"]).date
+        # confirm_data["time"] = pd.to_datetime(confirm_data["time"]).time
+
+        # [print(f'DATE: {d}') for d in confirm_data["date"]]
+        # [print(f'TIME: {d}') for d in confirm_data["time"]]
+        # [print(f'HOURS: {d}') for d in hours]
+
+        # print([confirm_data["time"].min(), confirm_data["time"].max()])
+
+        # [print(f'TIME: {d}') for d in confirm_data["time"]]
             
         fig = px.scatter(
             confirm_data, 
@@ -44,12 +65,22 @@ def generateReport(conn: psycopg2.extensions.connection, medName: str,) -> str:
 
         fig.update_yaxes(range=[0,2400])
 
-        # fig.update_layout(
-        #     yaxis={
-        #         'type': 'date'   
-        #     }
-        #     # yaxis_tickformat = '%H:%M'
-        # )
+        fig.update_layout(
+            xaxis={
+                'type': 'date',
+            },
+            # yaxis={
+            #     'type': 'date',
+            #     # 'tickformat': '%H:%M',
+            #     # 'nticks': 24,
+            #     # 'tick0': hours[0],
+            #     # 'dtick': 3600000,
+            #     # 'range': [hours[0], hours[len(hours) - 1]],
+            #     # 'autorange': False
+            # }
+            # xaxis_tickformat = '%B %d\n%Y'
+            # yaxis_tickformat=f'%X'
+        )
 
         fig.update_traces(marker=dict(size=18,
                                     line=dict(width=2,
