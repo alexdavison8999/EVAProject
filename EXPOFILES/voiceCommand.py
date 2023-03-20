97c3c44200b2d69f80d9edbc4150342e1243f9e6
Ryan-Kunkel <ryanjkunkel@gmail.com>
2023-03-20 16:43:13 -0400

    Added scanBottle VC navigation

--- EXPOFILES/voiceCommand.py
+++ EXPOFILES/voiceCommand.py
@@ -1,10 +1,16 @@
+from __future__ import annotations
+
 import speech_recognition as sr 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from UIController import UIController
 
 # Initialize the speech recognition 
 r = sr.Recognizer() 
 
 # Create a function that will be called when the command is given  
-def record_speech(): 
+def record_speech(UIController: UIController): 
 	# Record the command from the microphone 
 	with sr.Microphone(device_index=1) as source: 
 		print("Listening...") 
@@ -14,6 +20,7 @@ def record_speech():
 	try: 
 		# Use the Google Speech Recognition API 
 		command = r.recognize_google(audio).lower() 
+		navigateMenu(UIController, command)
 		print("You said: " + command + "\n") 
 	
 	# Error handling 
@@ -22,5 +29,11 @@ def record_speech():
 	
 	return command 
 
+def navigateMenu(UIController, voiceStr):
+	if 'scan' in voiceStr:
+		print("navigating to scan")
+		UIController.goToScanBottle()
+	else:
+		print("try again")
 # Call the function 
 # command()
