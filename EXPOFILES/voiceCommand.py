872c28d26644fe970eb84680bdeda222af52967b
Ryan-Kunkel <ryanjkunkel@gmail.com>
2023-03-29 16:28:48 -0400

    Potential fix to medicine call

--- EXPOFILES/voiceCommand.py
+++ EXPOFILES/voiceCommand.py
@@ -25,7 +25,8 @@ def record_speech(UIController: UIController, medications):
 	
 	try: 
 		# Use the Google Speech Recognition API 
-		command = r.recognize_google(audio).lower() 
+		command = r.recognize_google(audio).lower()
+		command = command.capitalize()
 		navigateMenu(UIController, command, medications)
 		print("You said: " + command + "\n") 
 	
