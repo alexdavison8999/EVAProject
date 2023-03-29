c335d4371b9ab32d42676a80457d9de9e6b6a1d7
Peter Kroeger <peter14mailgmail.com>
2023-03-29 14:29:18 -0400

    feat(drugInfo): Adjust title, add font and color

--- EXPOFILES/drugInfoGui.py
+++ EXPOFILES/drugInfoGui.py
@@ -16,46 +16,63 @@ if TYPE_CHECKING:
 
 
 def goToCommand(UIController, medName):
-	print("Click")
-	individualDrug(UIController, medName=medName)
-	pass
+    print("Click")
+    individualDrug(UIController, medName=medName)
+    return
+
 
 def loadingDrugGui(UIController: UIController):
+    # This is the section of code which creates the a label
+    d_info_label = tk.Label(
+        UIController.canvas,
+        text="Drug Info",
+        bg=os.getenv("PRIMARY_COLOR"),
+        font=(os.getenv("TEXT_FONT"), 40, "normal"),
+    )
 
-	# This is the section of code which creates the a label
-	d_info_label = tk.Label(UIController.canvas, text='Drug Info', bg='#F0F8FF', font=('arial', 40, 'normal'))
+    go_back_btn = UI.NewExitBtn(
+        master=UIController.canvas, text="Go Back", command=UIController.goToHome
+    )
 
-	go_back_btn = UI.NewExitBtn(master=UIController.canvas, text='Go Back', command=UIController.goToHome)
+    UIController.canvasIds["DrugInfo"] = []
 
-	UIController.canvasIds["DrugInfo"] = []
- 
-	medications = medicationsQuery(UIController.conn)
-	button_frame = tk.Frame(UIController.canvas, background=os.getenv("PRIMARY_COLOR"), border=1)
-	button_frame.grid(row=0, column=0, sticky="e", rowspan=len(medications))
- 
-	for index, med in enumerate(medications):
-		UI.NewMedBtn(
-			master=button_frame, 
-			text=f'{med.medName}', 
-			command= functools.partial(goToCommand, UIController, med.medName)
-		).grid(row=index, column=0, pady=GRID_PADDING)
-	
-	eva_face = UI.evaFace(file="EXPOFILES/assets/evaFaceRedLarge.png")
-	eva_text = UI.evaText(
-        canvas=UIController.canvas, 
-        text="Select a \nmed for information"
-	)
-	UIController.canvasIds["DrugInfo"].append(UIController.canvas.create_window(
-        275, WINDOW_HEIGHT / 5, window=eva_text
-    ))
-	UIController.canvasIds["DrugInfo"].append(UIController.canvas.create_window(
-        275, WINDOW_HEIGHT / 2, window=eva_face
-    ))
-	UIController.canvasIds["DrugInfo"].append(UIController.canvas.create_window(
-        WINDOW_WIDTH_PADDING, WINDOW_HEIGHT / 4, window=button_frame, anchor=tk.E
-    ))
- 
-	UIController.canvasIds["DrugInfo"].append(UIController.canvas.create_window(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 20, window=d_info_label, anchor=tk.N))
-	
-	UIController.canvasIds["DrugInfo"].append(UIController.canvas.create_window(WINDOW_PADDING, WINDOW_HEIGHT_PADDING, window=go_back_btn, anchor=tk.SW))
-	
+    medications = medicationsQuery(UIController.conn)
+    button_frame = tk.Frame(
+        UIController.canvas, background=os.getenv("PRIMARY_COLOR"), border=1
+    )
+    button_frame.grid(row=0, column=0, sticky="e", rowspan=len(medications))
+
+    for index, med in enumerate(medications):
+        UI.NewMedBtn(
+            master=button_frame,
+            text=f"{med.medName}",
+            command=functools.partial(goToCommand, UIController, med.medName),
+        ).grid(row=index, column=0, pady=GRID_PADDING)
+
+    eva_face = UI.evaFace(file="EXPOFILES/assets/evaFaceRedLarge.png")
+    eva_text = UI.evaText(
+        canvas=UIController.canvas, text="Select a \nmed for information"
+    )
+    UIController.canvasIds["DrugInfo"].append(
+        UIController.canvas.create_window(275, WINDOW_HEIGHT / 5, window=eva_text)
+    )
+    UIController.canvasIds["DrugInfo"].append(
+        UIController.canvas.create_window(275, WINDOW_HEIGHT / 2, window=eva_face)
+    )
+    UIController.canvasIds["DrugInfo"].append(
+        UIController.canvas.create_window(
+            WINDOW_WIDTH_PADDING, WINDOW_HEIGHT / 3, window=button_frame, anchor=tk.E
+        )
+    )
+
+    UIController.canvasIds["DrugInfo"].append(
+        UIController.canvas.create_window(
+            WINDOW_WIDTH / 2, WINDOW_HEIGHT / 20, window=d_info_label, anchor=tk.N
+        )
+    )
+
+    UIController.canvasIds["DrugInfo"].append(
+        UIController.canvas.create_window(
+            WINDOW_PADDING, WINDOW_HEIGHT_PADDING, window=go_back_btn, anchor=tk.SW
+        )
+    )
